5b14659ae99d245a4240da8f04df3a7940b7ed03
Ales Nabernik <nabernik.ales@gmail.com>
2022-02-19 13:10:41 +0100

    DB Backup Script + Backup

--- Scripts/testFile.py
+++ Scripts/testFile.py
@@ -1,13 +1,32 @@
-from plexapi.myplex import MyPlexAccount
+import pyodbc
 import configparser
+from datetime import datetime
+
+def dbConnect(sqlServerName, database):
+    conn = pyodbc.connect(
+        'Driver={SQL Server};'
+        'Server=' + sqlServerName + ';'
+        'Database=' + database + ';'
+        'Trusted_Connection=yes;'
+    )
+    return [conn, conn.cursor()]
 
 config = configparser.ConfigParser()
 config.read("config.ini")
+databaseConfig = config["Database"]
+sqlServerName = databaseConfig["serverName"]
+database = databaseConfig["database"]
 
-plexCredentials = config["PlexCredentials"]
-username = plexCredentials["username"]
-password = plexCredentials["password"]
-serverName = plexCredentials["serverName"]
-account = MyPlexAccount(username, password)
-plex = account.resource(serverName).connect()
-animeLibrary = plex.library.section("Anime")
+db = dbConnect(sqlServerName, database)
+conn = db[0]
+cursor = db[1]
+
+currentDatetime = datetime.now()
+formattedCurrentDatetime = currentDatetime.strftime("%d-%m-%Y %H_%M_%S")
+
+backup = rf"backup database [master] to disk = N'C:\Users\Nabernik\Desktop\GitHub\xdccDownloader\DB Backups\masterBKP_{formattedCurrentDatetime}.bak'"
+conn.autocommit = True
+cursor.execute(backup)
+while cursor.nextset():
+    pass
+conn.close()
